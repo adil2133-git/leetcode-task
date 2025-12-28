e722991740edfa767f1a09222f5b60a401041818
adil2133-git <adilaadi340@gmail.com>
2025-12-28 18:19:41 +0530

    q1672,q2396

---
+++ Q1672.js
@@ -0,0 +1,19 @@
+var maximumWealth = function(accounts) {
+    let richest = 0;
+
+    for(let i=0;i<accounts.length;i++){
+        let sum = 0;
+        
+        for(let j=0;j<accounts[i].length;j++){
+            sum += accounts[i][j]
+        }
+        if(sum>richest){
+            richest = sum
+        }
+    }
+    return richest
+}
+
+console.log(maximumWealth([[1,2,3],[3,2,1]]))
+console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+console.log(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
---
+++ Q2396.js
@@ -0,0 +1,6 @@
+var isStrictlyPalindrome = function(n) {
+    return false
+}
+
+console.log(isStrictlyPalindrome(9))
+console.log(isStrictlyPalindrome(4))
