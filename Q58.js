bcce13c47f7494dcb28b512897043811585813ef
adil2133-git <adilaadi340@gmail.com>
2026-01-02 11:22:06 +0530

    q58 and q1

---
+++ Q58.js
@@ -0,0 +1,10 @@
+var lengthOfLastWord = function(str){
+    let arr = str.trim().split(" ")
+    let lastWord = arr[arr.length-1]
+    let result = lastWord.length
+    return result
+}
+
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(lengthOfLastWord("luffy is still joyboy"))
