d13f34f15a80cfc99fb84484535afee3be07bade
adil2133-git <adilaadi340@gmail.com>
2026-06-29 10:59:39 +0530

    283

--- Q283.js
+++ Q283.js
@@ -1,3 +1,13 @@
-var moveZeroes = function(nums) {
-    
+var moveZeroes = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length - 1; j++) {
+            if (nums[j] === 0) {
+                let temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temp
+            }
+        }
+    }
+    return(nums)
 };
+console.log(moveZeroes([0,1,0,3,12]))
